0c0d23fee6b9ee761c46eb1f9d238a1d76644b8b
lpdft <772346730@qq.com>
2020-02-23 13:16:26 +0800

    [ruyue-zhang] feat: add Teacher class attribute and function

--- map/src/main/java/Teacher.java
+++ map/src/main/java/Teacher.java
@@ -1,3 +1,15 @@
-public class Teacher {
+import java.util.Map;
 
+public class Teacher {
+    private String name;
+    private Map<String, Integer> student;
+
+    public Teacher(String name, Map<String, Integer> student) {
+        this.name = name;
+        this.student = student;
+    }
+
+    public void addStudent(Student stu) {
+        student.put(stu.getName(), stu.getAge());
+    }
 }
