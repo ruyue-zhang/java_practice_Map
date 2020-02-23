f97e089b4a05a6e756b6ad3c3615097cd4f0bf42
lpdft <772346730@qq.com>
2020-02-23 13:28:03 +0800

    [ruyue-zhang] feat: add Application create Teacher and add Students

--- map/src/main/java/Application.java
+++ map/src/main/java/Application.java
@@ -1,6 +1,16 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class Application {
 
   public static void main(String[] args) {
+    Map<String, Integer> student = new LinkedHashMap<>();
+    student.put("Linda", 18);
+    student.put("Cindy", 19);
+    student.put("Mary", 19);
+    Teacher bob = new Teacher("Bob", student);
+    bob.addStudent(new Student("Ben", 19));
+    bob.addStudent(new Student("Solider", 20));
   }
 
 }
